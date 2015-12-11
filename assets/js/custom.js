df44200e8336a09868547310b5ad016cfe1644b3
ekudriashov <4150878@gmail.com>
2015-12-11 22:21:59 +0200

    add simple ajax form handling

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -25,4 +25,44 @@ jQuery(document).ready(function($) {
         }, scroll_top_duration
       );
     });
+
+    //Contact form ajax handling
+    var form = $('#ajax-contact');
+    var formMessages = $('#form-messages');
+    $(form).submit(function(event) {
+    	event.preventDefault();
+    	// Serialize the form data.
+		var formData = $(form).serialize();
+		// Submit the form using AJAX.
+		$.ajax({
+		    type: 'POST',
+		    url: $(form).attr('action'),
+		    data: formData
+		})
+		.done(function(response) {
+		    // Make sure that the formMessages div has the 'success' class.
+		    $(formMessages).removeClass('hide');
+		    $(formMessages).addClass('success');
+
+		    // Set the message text.
+		    $(formMessages).text(response);
+
+		    // Clear the form.
+		    $('#name').val('');
+		    $('#email').val('');
+		    $('#message').val('');
+		})
+		.fail(function(data) {
+		    // Make sure that the formMessages div has the 'error' class.
+		    $(formMessages).removeClass('hide');
+		    $(formMessages).addClass('error');
+
+		    // Set the message text.
+		    if (data.responseText !== '') {
+		        $(formMessages).text(data.responseText);
+		    } else {
+		        $(formMessages).text('Oops... Something wrong! Try again later pls!');
+		    }
+		});
+    });
 });
